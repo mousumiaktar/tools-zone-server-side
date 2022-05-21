8a5dc286bf24f137b85739de2381d52a6ae621d3
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-22 00:43:09 +0600

    basic set up

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayosb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running!');
+  })
+  
+  app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+  })
