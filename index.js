40184b936a49000ac2d39826cf169c2889453743
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-22 03:02:44 +0600

    get api added

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try{
+        await client.connect();
+        const toolCollection = client.db('toolShop').collection('tool');
+
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('server is running!');
